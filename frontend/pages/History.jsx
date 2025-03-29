c5939e46adc4d4d1ddf1031e313d50d7b4b99261
saisiddeswar <siddeswar0605@gmail.com>
2025-03-29 12:25:07 +0530

    Updated HistoryForm.jsx

--- frontend/pages/History.jsx
+++ frontend/pages/History.jsx
@@ -11,7 +11,7 @@ const History = () => {
         const fetchHistory = async () => {
             setLoading(true);
             try {
-                const response = await fetch(`https://anna-samarpan.vercel.app/api/food/history?username=${username}`, {
+                const response = await fetch(`https://annasamarpan-backend.onrender.com/api/food/history?username=${username}`, {
                     method: 'GET',
                     headers: { 'Content-Type': 'application/json' },
                 });
